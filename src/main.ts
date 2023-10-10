99d38d8569218add756e97ab82ec03405ce14579
number-nine <sergii.pokhylko@gmail.com>
2023-10-10 16:35:06 +0300

    1-Singleton

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+/** Singleton **/
+
+// import { Profile } from './singleton'
+
+// const profile1 = new Profile({ login: 'tom@email.com', name: 'Tom' })
+// console.log(profile1.user.name, profile1.postsCount);
+// profile1.incrementByValue(10);
+// console.log(profile1.user.name, profile1.postsCount);
+// const profile2 = new Profile({ login: "jack@email.com", name: "Jack" });
+// console.log(profile2.user.name, profile2.postsCount);
+
--- src/singleton/index.ts
+++ src/singleton/index.ts
@@ -1,3 +1,38 @@
+type User = {
+  login: string;
+  name?: string;
+};
 
+/** Singleton 1.1 **/
 
+// let instance: Profile;
 
+// export class Profile {
+//     postsCount: number = 0;
+//     constructor(public user:User) {
+//         if (!instance) instance = this;
+//         return instance
+//     }
+//     incrementByValue(value:number) {
+//         this.postsCount += value;
+//     }
+// }
+
+/** Singleton 1.2 **/
+
+// export class Profile {
+//   private static instance:Profile;
+//   postsCount:number = 0;
+
+//   constructor(public user: User) {
+//     if (typeof Profile.instance === 'object') {
+//       return Profile.instance;
+//     }
+//       Profile.instance = this;
+//       return this;
+//   }
+
+//   incrementByValue(value:number) {
+//     this.postsCount += value;
+//   }
+// }
